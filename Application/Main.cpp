09c13399849a2993758116edd88018862e21d238
bfinne05 <100608199+bfinne05@users.noreply.github.com>
2022-10-06 08:57:04 -0600

    Added Open Gl to the project

--- Application/Main.cpp
+++ Application/Main.cpp
@@ -1,7 +1,7 @@
 #include "Engine.h" 
 #include <iostream> 
 
-int main()
+int main(int argc, char** argv)
 {
 	neu::InitializeMemory();
 
@@ -9,6 +9,9 @@ int main()
 	neu::Engine::Instance().Register();
 
 	neu::g_renderer.CreateWindow("Neumont", 800, 600);
+
+	float angle = 0.0f;
+	neu::Vector2 position;
 
 	bool quit = false;
 	while (!quit)
@@ -18,8 +21,51 @@ int main()
 		if (neu::g_inputSystem.GetKeyState(neu::key_escape) == neu::InputSystem::KeyState::Pressed) quit = true;
 
 		neu::g_renderer.BeginFrame();
+		if (neu::g_inputSystem.GetKeyState(neu::key_left) == neu::InputSystem::KeyState::Held)
+		{
+			position.x -= neu::g_time.deltaTime;
+		}
+
+		if (neu::g_inputSystem.GetKeyState(neu::key_right) == neu::InputSystem::KeyState::Held)
+		{
+			position.x += neu::g_time.deltaTime;
+		}
+
+		if (neu::g_inputSystem.GetKeyState(neu::key_down) == neu::InputSystem::KeyState::Held)
+		{
+			position.y -= neu::g_time.deltaTime;
+		}
+
+		if (neu::g_inputSystem.GetKeyState(neu::key_up) == neu::InputSystem::KeyState::Held)
+		{
+			position.y += neu::g_time.deltaTime;
+		}
+
+		angle += 90.0f * neu::g_time.deltaTime;
+
+		glPushMatrix();
+
+		glScalef(0.5f, 0.5f, 0.5f);
+		//glRotatef(angle, 0, 0, 1);
+		glTranslatef(position.x, position.y, 0);
+		glBegin(GL_TRIANGLES);
+
+
+		glColor3f(1.0f, 0.0f, 0.0f);
+		glVertex2f(-1.0f, -1.0f);
+		//glColor3f(0.0f, 1.0f, 0.0f);
+		glVertex2f(0.0f, 1.0f);
+		//glColor3f(0.0f, 0.0f, 1.0f);
+		glVertex2f(1.0f, -1.0f);
+
+		glEnd();
+
+		glPopMatrix();
+
 		neu::g_renderer.EndFrame();
 	}
 
 	neu::Engine::Instance().Shutdown();
+
+	return 0;
 }
--- Engine/Renderer/Renderer.h
+++ Engine/Renderer/Renderer.h
@@ -2,9 +2,12 @@
 #include "Math/Vector2.h"
 #include "Math/Color.h"
 #include "Math/Matrix3x3.h"
+#include <glad/glad.h> 
+#include <SDL.h> 
 
 struct SDL_Renderer;
 struct SDL_Window;
+
 
 namespace neu
 {
@@ -55,5 +58,6 @@ namespace neu
 
 		SDL_Renderer* m_renderer = nullptr;
 		SDL_Window* m_window = nullptr;
+		SDL_GLContext m_context;
 	};
 }
